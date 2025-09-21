5cddda6833525c6ee1a9104663f091c96fa68265
R Sribalaji <pablosribcse@gmail.com>
2025-09-21 12:41:35 +0530

    21 Sep PS4 commit

--- PS4/q10.cpp
+++ PS4/q10.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
 using namespace std;
+class Fraction
+{
+    float numerator;
+    float denominator;
 
-int main() {
+public:
+    Fraction()
+    {
+        this->numerator = 0;
+        this->denominator = 1;
+    }
+    Fraction(float num)
+    {
+        this->numerator = num;
+        this->denominator = 1;
+    }
+    Fraction(float num, float Den)
+    {
+        this->numerator = num;
+        this->denominator = Den;
+    }
+    void get_fraction_value(void)
+    {
+        if (this->denominator == 0)
+        {
+            cout << "Fraction is not defined, Denominator cannot be zero.\n";
+        }
+        else
+        {
+            cout << "Fractional form: " << this->numerator << "/" << this->denominator << endl;
+            cout << "Decimal form: " << this->numerator / this->denominator << endl;
+        }
+    }
+};
+int main()
+{
     // Your code here
+    Fraction f1, f2(2), f3(2, 3), f4(4, 0);
+    f1.get_fraction_value();
+    f2.get_fraction_value();
+    f3.get_fraction_value();
+    f4.get_fraction_value();
     return 0;
 }
--- PS4/q9.cpp
+++ PS4/q9.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 using namespace std;
+class Employee
+{
+    string name;
+    int id;
+    float salary;
 
-int main() {
-    // Your code here
+public:
+    Employee(string nam, int id)
+    {
+        this->name = nam;
+        this->id = id;
+        this->salary = 0;
+    }
+    Employee(string nam, int id, float sal)
+    {
+        this->name = nam;
+        this->id = id;
+        this->salary = sal;
+    }
+    void get(void)
+    {
+        cout << "Employee Name: " << this->name << "\nEmployee ID: " << this->id << "\nEmployee Salary: $" << this->salary << endl;
+    }
+};
+int main()
+{
+    Employee e1("Renga Punga", 12), e2("Arune Karune", 34, 100);
+    e1.get();
+    e2.get();
     return 0;
 }
